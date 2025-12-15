0c0004fe27d675648419d6270c074686cda73a81
Louaf <albaqaryyousif@gmail.com>
2025-12-15 16:46:04 +0200

    login logic

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -44,6 +44,32 @@ router.post("/register", (req, res) => {
     res.sendStatus(503);
   }
 });
-router.post("/login", (req, res) => {});
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const getUser = db.prepare(`
+      SELECT * FROM users WHERE username = ? 
+      `);
+    const user = getUser.get(username);
+    //unmatched email or username
+    if (!user) {
+      return res.status(404).send({ message: "user not found" });
+    }
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    // unmatched password
+    if (!passwordIsValid) {
+      return res.status(401).send({ message: "invalid password! " });
+    }
+    console.log(user);
+    //successful authentecation
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    res.json({ token });
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(503);
+  }
+});
 
 export default router;
